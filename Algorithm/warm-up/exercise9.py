4d15f17b46e67680131e3b6b03e857a9709f8d0a
dskusuma <dwisatriakusuma4@gmail.com>
2018-05-12 00:59:19 +0700

    Revert "finished warm up"
    This reverts commit 19384f7c58988d521165e68123d5cf0e1b5db1ef.

--- Algorithm/warm-up/exercise9.py
+++ Algorithm/warm-up/exercise9.py
@@ -11,14 +11,12 @@ def birthdayCakeCandles(n, ar):
     for i in range(0,n):
         if ar[i]>tertinggi:
             tertinggi = ar[i]
-            jumlah=1
         elif tertinggi == ar[i]:
             jumlah+=1
     
     return(jumlah)
 
-if __name__ == "__main__":
-    n = int(raw_input().strip())
-    ar = map(int, raw_input().strip().split(' '))
-    result = birthdayCakeCandles(n, ar)
-    print(result)
+n = int(raw_input().strip())
+ar = map(int, raw_input().strip().split(' '))
+result = birthdayCakeCandles(n, ar)
+print(result)
